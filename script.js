9875f52eb6f99a9d423fa5154b41de2304e2647e
pabletefatimarocio <pabletefatima9@gmail.com>
2023-10-08 19:53:45 -0300

    asd

---
+++ script.js
@@ -0,0 +1,9 @@
+var canvas = document.createElement('canvas');
+var canvasDiv = document.getElementById('canvas1');
+
+canvas.width = 400; 
+canvas.height = 200; 
+
+canvasDiv.appendChild(canvas);
+
+
